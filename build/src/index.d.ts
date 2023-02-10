1f36260d405168d2bdb64f8704207769278e92cc
kobrien <kobrien@activated.io>
2023-02-09 17:31:36 -0800

    Updated exported interfaces

--- build/src/index.d.ts
+++ build/src/index.d.ts
@@ -2,4 +2,4 @@ import { IAuthentication } from './Authentication';
 import { IAuthKit, ICreateParams } from './AuthKit';
 import { ITokens } from './Tokens';
 declare const createAuthKitForDOM: (params: ICreateParams) => IAuthKit;
-export { createAuthKitForDOM, IAuthKit, IAuthentication, ITokens };
+export { createAuthKitForDOM, IAuthKit, IAuthentication, ITokens, ICreateParams };
